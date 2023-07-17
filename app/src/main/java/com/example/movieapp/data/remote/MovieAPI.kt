eed35c948dbf5b20c84a0e8c49ecb7a05b8c983d
İbrahim TAŞKIN <taskinnibrahim@gmail.com>
2023-07-17 05:07:26 +0300

    Version 1.0

---
+++ app/src/main/java/com/example/movieapp/data/remote/MovieAPI.kt
@@ -0,0 +1,26 @@
+package com.example.movieapp.data.remote
+
+import com.example.movieapp.data.remote.dto.MovieDetailDto
+import com.example.movieapp.data.remote.dto.MoviesDto
+import com.example.movieapp.util.Constants.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieAPI {
+
+    //https://www.omdbapi.com/?apikey=882800fe&s=batman
+    //https://www.omdbapi.com/?apikey=882800fe&i=tt3896198
+
+    @GET(".")
+    suspend fun getMovies(
+        @Query("s") searchString: String,
+        @Query("apikey") apiKey : String = API_KEY
+    ) : MoviesDto
+
+    @GET(".")
+    suspend fun getMovieDetail(
+        @Query("i") imdbId: String,
+        @Query("apikey") apiKey : String = API_KEY
+    ) : MovieDetailDto
+
+}
---
+++ app/src/main/java/com/example/movieapp/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.movieapp.util
+
+object Constants {
+    const val API_KEY = "882800fe"
+    const val BASE_URL = "http://www.omdbapi.com"
+    const val IMDB_ID = "imdb_id"
+}
